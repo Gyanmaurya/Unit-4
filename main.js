8719aef229710a74bff811fa0d465cf9cf502f46
Gyanmaurya <gyanprakashme0051@gmail.com>
2022-03-02 23:35:13 +0530

    Ready
    done

---
+++ main.js
@@ -0,0 +1,8 @@
+const add=require("./add.js");
+const multiply=require("./multiply.js");
+const divide=require("./divide.js");
+const subtract=require("./subtract.js");
+console.log(subtract(50,20))
+console.log(divide(20,2))
+console.log(multiply(3,5))
+console.log(add(7,7));
